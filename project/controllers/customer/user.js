9776109af80e7f72dc7eb72fcf2c78b3e23546f5
choyonpk <62893007+choyonpk@users.noreply.github.com>
2020-11-22 02:28:37 +0600

    personal info update

--- project/controllers/customer/user.js
+++ project/controllers/customer/user.js
@@ -5,12 +5,68 @@ const router = express.Router();
 
 router.get('/',(req,res)=>
 {
-    res.render('./customer/userhome');
+    if(req.cookies['username'] == null)
+    {
+        res.redirect('/login');   
+    }
+    else
+    {
+       
+        res.render('./customer/userhome');
+        
+       
+    }
 })
 
 router.get('/update',(req,res)=>
 {
-    res.render('./customer/userupdate');
+    if(req.cookies['username'] == null)
+    {
+        res.redirect('/login');   
+    }
+    else
+    {
+        var user ={
+            username:req.cookies['username']
+        }
+        um.getByUserName(user,(results)=>{
+            results = {
+                name:results.Name,
+                email:results.Email,
+                contactno:results.ContactNO,
+                address:results.Address    
+            }
+
+            res.render('./customer/userupdate',results);
+        })
+    }
+})
+router.post('/update',(req,res)=>
+{
+    if(req.cookies['username'] == null)
+    {
+        res.redirect('/login');   
+    }
+    else
+    {
+        var user = {
+            name:req.body.name,
+            email:req.body.email,
+            username:req.cookies['username'],
+            address:req.body.address,
+            contactno:req.body.phone,
+        };
+        um.updateCustomer(user,(status)=>{
+            if(status)
+            {
+                res.redirect('/userhome');
+            }
+            else
+            {
+                res.redirect('/userhome/update');
+            }
+        });
+    }
 })
 
 
--- project/models/userModel.js
+++ project/models/userModel.js
@@ -11,8 +11,15 @@ module.exports= {
 			}
 		});
 	},
-	getById: function(id, callback){
-
+	getByUserName: function(user, callback){
+		var sql = "select * from user where UserName='"+user.username+"'";
+		db.getResults(sql, function(results){
+			if(results.length >0 ){
+				callback(results[0]);
+			}else{
+				callback(false);
+			}
+		});
 	},
 	getAll: function(user,callback){
 		var sql = "select * from user where UserType='"+user+"' ";
@@ -53,6 +60,17 @@ module.exports= {
 	
 		});
 
+	},
+	updateCustomer:function(user, callback){
+		var sql = "update user set Name = '"+user.name+"',ContactNo = '"+user.contactno+"',Email = '"+user.email+"',Address = '"+user.address+"' where UserName = '"+user.username+"'";
+		db.execute(sql, function(status){
+			if(status){
+				callback(true);
+			}else{
+				callback(false);
+			}
+	
+		});
 	}
 
 	
